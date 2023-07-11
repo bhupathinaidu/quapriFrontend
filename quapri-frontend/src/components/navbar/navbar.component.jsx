743ce6ffbc971271facf7bdaeb5b79bd09fc21b0
Nitesh Pandey <nitesh.pandey014@gmail.com>
2023-07-11 23:28:55 +0530

    CSS Changes

--- quapri-frontend/src/components/navbar/navbar.component.jsx
+++ quapri-frontend/src/components/navbar/navbar.component.jsx
@@ -1,19 +1,36 @@
 import React from "react";
-import "./navbar.component.css"
+import "./navbar.component.css";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 const NavbarComponent = () => {
-    return (
-        <nav className="navbarContainer">
-            <div className="navbarWrapper">
-                <a className="link" href="#">Customer Support</a>
-                <div className="otherNavLinks">
-                    <a className="link" href="#">Find Quapri Store</a>
-                    <a className="link" href="#">Sample Kit</a>
-                    <a className="businessLink" href="#">Business Pricing</a>
-                </div>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <Navbar bg="light" expand="lg" className="desktop-navbar-menu">
+      <Container>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Customer Support
+              </Link>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <span className="nav-link"> Find Quapri Store</span>
+            </li>
+            <li>
+              <span className="nav-link">Sample Kit</span>
+            </li>
+            <li>
+              <span className="nav-link businessLink">Business Pricing</span>
+            </li>
+          </ul>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
