80b8e0832a936800a7a65370cfdec8df7cf15f36
Shubham singh <sksvnsss@gmail.com>
2023-10-01 03:20:01 +0530

    custom design

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import { testReducer } from "./test/test.reducer";
 import { searchReducer } from "./search/search.reducer";
+import { productReducer } from "./product/product.reducer";
 
 export const rootReducer = combineReducers({
     test: testReducer,
-    search: searchReducer
+    search: searchReducer,
+    productDetails: productReducer
 })
