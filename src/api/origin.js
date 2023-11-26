69cc168f1cbf6e6fcd199d3b6a44456f559599a9
Nitesh Pandey <nitesh.pandey014@gmail.com>
2023-11-26 21:37:51 +0530

    Origin Change

--- src/api/origin.js
+++ src/api/origin.js
@@ -1,1 +1,4 @@
-export const origin = () => process.env.NODE_ENV === "development"? "http://localhost:8000" : "https://sea-lion-app-lsbln.ondigitalocean.app";
+export const origin = () =>
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://quapri.onrender.com";
